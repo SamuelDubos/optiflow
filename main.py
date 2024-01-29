17f63279478c9c01596290b25203ffaded3374b1
Samuel DUBOS <samuel.dubos11@laposte.net>
2024-01-29 15:32:04 +0100

    Update main (parser)

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from src.pixel_tracking import PixelTracker
 from src.zone_delimiting import ZoneDelimiter
 from src.poi_identifying import PoiIdentifier
 from src.poi_tracker import PoiTracker
+from seq.matcher import Photographer, PhotographsMatcher
 
 
 class Tracker:
@@ -42,7 +43,7 @@ class Tracker:
         parser.add_argument('--camera', type=int, default=0,
                             help='Select the camera peripheral '
                                  '(0 for built-in webcam, 1 for external webcam)')
-        parser.add_argument('--item', type=str, required=True,
+        parser.add_argument('--item', type=str, default='poi',
                             help='Select the type of time to track (pixel, zone, or poi)')
 
         # Specific arguments
@@ -52,6 +53,10 @@ class Tracker:
                             help='Add Points Of Interest (only for item == poi)')
         parser.add_argument('--track', action='store_false',
                             help='Track the Points Of Interest (only for item == poi)')
+        parser.add_argument('--folder', type=str, default='',
+                            help='Select the folder (only for item == limits)')
+        parser.add_argument('--run', action='store_false',
+                            help='Take new screenshots (only for item == limits)')
         return parser.parse_args()
 
     def main(self):
@@ -62,12 +67,21 @@ class Tracker:
         if self.args.item == 'pixel':
             PixelTracker(self.args.camera).main()
         elif self.args.item == 'zone':
-            ZoneDelimiter(self.args.camera, self.args.mesh).main()
+            ZoneDelimiter(self.args.camera,
+                          self.args.mesh).main()
         elif self.args.item == 'poi':
             if self.args.track:
-                PoiTracker(self.args.camera, self.args.n_poi).main()
+                PoiTracker(self.args.camera,
+                           self.args.n_poi).main()
             else:
-                PoiIdentifier(self.args.camera, self.args.n_poi).main()
+                PoiIdentifier(self.args.camera,
+                              self.args.n_poi).main()
+        elif self.args.item == 'limits':
+            photographer = Photographer(camera=self.args.camera,
+                                        folder=self.args.folder,
+                                        run=self.args.run)
+            PhotographsMatcher(camera=self.args.camera,
+                               photographer=photographer).main()
 
 
 if __name__ == '__main__':
